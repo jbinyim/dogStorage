609c5ce2efd3499de4ed5530e91d67b6bf67e90f
jbinyim <jbinyim12@naver.com>
2024-11-05 18:36:50 +0900

    241105 게시판 생성중

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,14 +1,77 @@
-import React from "react";
-import styled from "styled-components";
+import { collection, onSnapshot, orderBy, query } from "firebase/firestore";
+import React, { useEffect, useState } from "react";
+import { db } from "../firebase";
+import { Unsubscribe } from "firebase/auth";
+import {
+  Box,
+  Day,
+  Desc,
+  Title,
+  User,
+  Video,
+  Wrapper,
+  Write,
+} from "../styles/boardStyle";
 
-const Wrapper = styled.div`
-  max-width: 1300px;
-  width: 100%;
-  margin: 0 auto;
-`;
+interface IBoard {
+  id: string;
+  board: string;
+  date: string;
+  title: string;
+  userId: string;
+  username: string;
+  youtbue?: string;
+}
 
 const Board = () => {
-  return <Wrapper>Board</Wrapper>;
+  const [board, setBoard] = useState<IBoard[]>([]);
+
+  useEffect(() => {
+    let unsubscribe: Unsubscribe | null = null;
+
+    const fetchBoards = async () => {
+      const boardQuery = query(
+        collection(db, "board"),
+        orderBy("date", "desc")
+      );
+
+      unsubscribe = await onSnapshot(boardQuery, (spanshop) => {
+        const boards = spanshop.docs.map((item) => {
+          const { board, date, title, userId, username, youtbue } = item.data();
+          return {
+            board,
+            date,
+            title,
+            userId,
+            username,
+            youtbue,
+            id: item.id,
+          };
+        });
+        setBoard(boards);
+      });
+    };
+    fetchBoards();
+    return () => {
+      unsubscribe && unsubscribe();
+    };
+  }, []);
+
+  return (
+    <Wrapper>
+      <Box>
+        {board.map((docs) => (
+          <Write>
+            <Title>{docs.title}</Title>
+            <Day>{docs.date}</Day>
+            <Desc>{docs.board}</Desc>
+            <Video>{docs.youtbue}</Video>
+            <User>{docs.username}</User>
+          </Write>
+        ))}
+      </Box>
+    </Wrapper>
+  );
 };
 
 export default Board;
---
+++ src/styles/boardStyle.ts
@@ -0,0 +1,31 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  max-width: 1300px;
+  width: 100%;
+  margin: 0 auto;
+`;
+
+export const Box = styled.div`
+  display: flex;
+  grid-template-columns: repeat(4, 1fr);
+  gap: 20px;
+`;
+
+export const Write = styled.div`
+  width: 300px;
+  height: 340px;
+  border: 1px solid #000;
+  border-radius: 30px;
+  padding: 20px 25px 0;
+`;
+
+export const Title = styled.h2``;
+
+export const Desc = styled.p``;
+
+export const Video = styled.div``;
+
+export const Day = styled.p``;
+
+export const User = styled.p``;
